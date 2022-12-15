0b4836bbd6f60433e3e380fd07d5252896f926b1
abdusamad8575 <samadpts786313@gmail.com>
2022-12-15 21:04:06 +0000

    prime

---
+++ script.js
@@ -0,0 +1,8 @@
+function displayBrowser(){
+    var clic=document.getElementById('ulClass1');
+    if(clic.style.display === 'none'){
+        clic.style.display = 'block';
+    }else{
+        clic.style.display = 'none';
+    }
+}
